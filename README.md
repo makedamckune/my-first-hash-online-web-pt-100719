9766dce970c249fb9b6bf55a55e2d535b260b3e1
Makeda P Mckune <makedamckune@gmail.com>
2019-10-13 00:40:47 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# My First Hash
+  My First Hash
 
 ## Objectives
 
--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,13 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+ my_hash = {
+   "name" => "Makeda Mckune", 
+    "age" => "23"
+   "place of birth" => "Brooklyn"
+ }
+ 
+ 
+ 
+ 
   
 end
 
